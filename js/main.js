90f2b780c03668bcc0869edb386d32b9ae3c7a0b
Marco <mr.lepore@hotmail.it>
2021-03-22 16:39:23 +0100

    correzioni di Filippo (per corretta grammatica relativa ad array di oggetti)

--- js/main.js
+++ js/main.js
@@ -2,16 +2,62 @@ var root = new Vue({
     el: '#root',
     data: {
     counter_photo: 0,
-    images: [
-        'images/bulbasaur.png',
-        'images/ivysaur.png',
-        'images/venusaur.png'
-    ],
-    names: [
-        'Bulbasaur',
-        'Ivysaur',
-        'Venusaur',
-    ],
+    pokemon: [
+        {
+            myName: 'Bulbasaur',
+            img: 'images/bulbasaur.png',
+            sign: 'earth',
+            img_sign: 'images/earth.jpeg'
+        },
+        {
+            myName: 'Ivysaur',
+            img: 'images/ivysaur.png',
+            sign: 'earth',
+            img_sign: 'images/earth.jpeg'
+        },
+        {
+            myName: 'Venusaur',
+            img: 'images/venusaur.png',
+            sign: 'earth',
+            img_sign: 'images/earth.jpeg'
+        },
+        {
+            myName: 'Charmender',
+            img: 'images/charmender.png',
+            sign: 'fire',
+            img_sign: 'images/earth.jpeg'
+        },
+        {
+            myName: 'Charmeleon',
+            img: 'images/charmeleon.png',
+            sign: 'fire',
+            img_sign: 'images/earth.jpeg'
+        },
+        {
+            myName: 'Charizard',
+            img: 'images/charizard.png',
+            sign: 'fire',
+            img_sign: 'images/earth.jpeg'
+        },
+        {
+            myName: 'Squirtle',
+            img: 'images/squirtle.png',
+            sign: 'water',
+            img_sign: 'images/earth.jpeg'
+        },
+        {
+            myName: 'Wartortle',
+            img: 'images/wartortle.png',
+            sign: 'water',
+            img_sign: 'images/earth.jpeg'
+        },
+        {
+            myName: 'Blastoise',
+            img: 'images/blastoise.png',
+            sign: 'water',
+            img_sign: 'images/earth.jpeg'
+        }
+    ]
     },
     /*created: function() {
         setInterval(() => {
@@ -22,17 +68,28 @@ var root = new Vue({
         prevClick: function() {
             this.counter_photo -= 1;
             if (this.counter_photo < 0) {
-                this.counter_photo = (this.images.lenght -1);
+                this.counter_photo = (this.pokemon.length -1);
             }
         },
         nextClick: function() {
             this.counter_photo += 1;
-            if (this.counter_photo > (this.images.length -1)) {
+            if (this.counter_photo > (this.pokemon.length -1)) {
                 this.counter_photo = 0;
             }
         },
         clickPoint: function() {
             
+        },
+        chooseColor: function(x) {
+            if (x == 'earth') {
+                return "green";
+            }
+            else if (x == 'fire') {
+                return 'red';
+            }
+            else if (x == 'water') {
+                return 'blue';
+            }
         }
     }
     });
